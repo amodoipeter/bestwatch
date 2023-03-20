a2f500604d0b6dbb9067aee599073334ca013532
Ethiopian_boy <mullerfromethiopia@gmail.com>
2023-03-20 16:58:03 +0530

    movie request in the home page

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
 
 # route = url = link
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    fetchedData = requests.get("https://www.omdbapi.com/?apikey=20f5e004&s=batman")
+    movies = fetchedData.json()
+    
+    return render_template("home.html", movies=movies)
 
 
 if __name__ == "__main__":
