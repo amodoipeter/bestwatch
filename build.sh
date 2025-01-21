2e9e1b9b6d4ec203458218279b90f3acad9cb95b
amodoi <amodoipeter@gmail.com>
2025-01-21 08:39:44 +0300

    update build file

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 apt-get update
+apt-get upgrade
+apt-get install -y python3
+apt-get install -y python3-pip
 apt-get install -y python3-dev
 pip install -r requirements.txt
 MYSQLCLIENT_CFLAGS=-I/path/to/mysqlclient
